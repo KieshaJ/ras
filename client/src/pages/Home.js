01d3ee5733e2909ee4935662116345738461c2ce
KieshaJ <duel.kj@gmail.com>
2022-01-20 23:27:42 +0200

    RAS / Home page finished and Survey submission page initialized.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,22 @@
 import React from "react";
+import {Button} from "@mui/material";
 
 const HomePage = () => {
-    return(
-        <div>Home</div>
+    return (
+        <div className="home">
+            <div className="homeContent">
+                <h1 className="darkPurpleBtn">RESTORANŲ APKLAUSŲ SISTEMA</h1>
+
+                <div className="homeButtons">
+                    <Button
+                        variant="outlined"
+                        className="darkPurpleBtn fullWidth"
+                    >
+                        PILDYTI APKLAUSĄ
+                    </Button>
+                </div>
+            </div>
+        </div>
     );
 };
 
